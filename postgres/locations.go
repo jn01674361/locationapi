bbe8bba2b733a9669bd4442a2cb6e0f7aa89673c
jn01674361 <joar.nykvist@gmail.com>
2020-06-20 19:35:18 +0200

    Add sql to functions

--- postgres/locations.go
+++ postgres/locations.go
@@ -1,16 +1,79 @@
 package postgres
 
-import "locationapi/structs"
+import (
+	"database/sql"
+	"fmt"
+	"locationapi/structs"
 
-type postgresRepository struct {
-	Address string
+	"github.com/google/uuid"
+)
+
+// Repository p
+type Repository struct {
+	db *sql.DB
 }
 
-func (p *postgresRepository) ListLocations() []structs.Location {
+// ListLocations l
+func (p Repository) ListLocations() []structs.Location {
 	res := make([]structs.Location, 0)
+	sqlStatement := "SELECT * FROM locations;"
+
+	var id uuid.UUID
+	var lat, lon float32
+	var name, locationType string
+
+	rows, err := p.db.Query(sqlStatement)
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		switch err := rows.Scan(&id, &lat, &lon, &name, &locationType); err {
+		case sql.ErrNoRows:
+			panic("No rows were returned!")
+		case nil:
+			loc := structs.Location{
+				ID: id,
+				Coordinates: structs.Coordinates{
+					Lat: lat,
+					Lon: lon,
+				},
+				Metadata: structs.Metadata{
+					// Type: locationType,
+					// Name: name,
+				},
+			}
+			res = append(res, loc)
+		default:
+			panic(err)
+
+		}
+	}
+
 	return res
 }
 
-func (p *postgresRepository) StoreLocation(structs.Location) {
+// StoreLocation s
+func (p Repository) StoreLocation(structs.Location) {
+
+	sqlStatement := `
+		INSERT INTO locations (id, latitude, longitude, location_name, location_type)
+		VALUES ($1, $2, $3, $4, $5)`
+	_, err := p.db.Exec(sqlStatement, uuid.New(), 10.2, 1.1, "King's Cross Station", "public")
+	if err != nil {
+		panic(err)
+	}
+
 	return
+}
+
+func buildSQL(ID uuid.UUID) string {
+	return fmt.Sprintf("SELECT * FROM locations WHERE id='%s';", ID)
+}
+
+// NewRepository n
+func NewRepository(database *sql.DB) Repository {
+	return Repository{
+		db: database,
+	}
 }

b7691162e9ee1545f443b8ecb3d2aa3723e6cbcd
jn01674361 <joar.nykvist@gmail.com>
2020-06-20 19:35:50 +0200

    Add logic to functions in server

--- http/server.go
+++ http/server.go
@@ -1,6 +1,9 @@
 package http
 
 import (
+	"encoding/json"
+	"locationapi/structs"
+	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -8,7 +11,8 @@ import (
 
 // LocationService is an interface for CRUD operations on locations.
 type LocationService interface {
-	GetLocations()
+	GetLocations() []structs.Location
+	PostLocation(structs.Location)
 }
 
 // Server serves
@@ -21,9 +25,32 @@ func (s *Server) Open() {
 	muxRouter := mux.NewRouter()
 
 	muxRouter.Handle("/", NotImplemented).Methods(http.MethodGet)
-	muxRouter.Handle("/locations", GetLocationsFunc).Methods(http.MethodGet)
+	muxRouter.Handle("/locations", s.getLocations()).Methods(http.MethodGet)
+	muxRouter.Handle("/locations", GetLocationsFunc).Methods(http.MethodPost)
 
 	http.ListenAndServe(":8080", muxRouter)
+}
+
+func (s Server) getLocations() http.HandlerFunc {
+	log.Println("about get locations")
+	return func(w http.ResponseWriter, r *http.Request) {
+		res := s.locationService.GetLocations()
+		j, err := json.Marshal(res)
+		if err != nil {
+			panic(err)
+		}
+		w.Write([]byte(j))
+	}
+
+}
+
+func (s Server) postLocations() http.HandlerFunc {
+	log.Println("about to post locations")
+	return func(w http.ResponseWriter, r *http.Request) {
+		s.locationService.PostLocation(structs.Location{})
+		w.Write([]byte("byte"))
+	}
+
 }
 
 // GetLocationsFunc gets locations.
@@ -37,4 +64,6 @@ var NotImplemented = http.HandlerFunc(func(w http.ResponseWriter, r *http.Reques
 })
 
 // NewServer returns a new router.
-func NewServer() Server { return Server{} }
+func NewServer(location LocationService) Server {
+	return Server{locationService: location}
+}

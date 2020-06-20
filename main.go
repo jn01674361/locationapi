0c09629cf40f7fe683231c36f0e1af6e7ddd956d
jn01674361 <joar.nykvist@gmail.com>
2020-06-20 19:35:38 +0200

    Connect to pg from main

--- main.go
+++ main.go
@@ -1,8 +1,65 @@
 package main
 
-import "locationapi/http"
+import (
+	"database/sql"
+	"fmt"
+	"locationapi/http"
+	"locationapi/locations"
+	"locationapi/postgres"
+	"log"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	postgresHost     = "postgres"
+	postgresPort     = 5432
+	postgresUser     = "locationapi"
+	postgresPassword = "postgres"
+	postgresDBname   = "locationapi"
+)
 
 func main() {
-	httpServer := http.NewServer()
+
+	db := setupPostgres(
+		postgresHost,
+		postgresUser,
+		postgresPassword,
+		postgresDBname,
+		postgresPort,
+	)
+	defer db.Close()
+
+	repo := postgres.NewRepository(db)
+	locationService := locations.NewLocationService(repo)
+
+	httpServer := http.NewServer(locationService)
 	httpServer.Open()
+}
+
+func setupPostgres(host, user, password, dbname string, port uint) *sql.DB {
+
+	postgresConfig := fmt.Sprintf(
+		"postgres://%s:%s@%s:%d/%s?sslmode=%s",
+		postgresUser,
+		postgresPassword,
+		postgresHost,
+		postgresPort,
+		postgresDBname,
+		"disable",
+	)
+	db, err := sql.Open("postgres", postgresConfig)
+	if err != nil {
+		log.Println("could not open connection to postgres")
+		panic(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Println("could not ping database")
+		panic(err)
+	}
+
+	log.Println("Successfully connected!")
+	return db
 }
